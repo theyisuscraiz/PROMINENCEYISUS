84ad880a6d05d8a6361e4c00c8717d983c20e4a5
theyisuscraiz <80612097+theyisuscraiz@users.noreply.github.com>
2024-07-05 03:40:17 +0000

    Mundo guardado 2024-07-05

--- server.py
+++ server.py
@@ -10,7 +10,7 @@
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
+# eu		      Europa (Fra1nkfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
